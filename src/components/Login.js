7e8b479a152b751c76228e1cb7ad0d5db0c11f6d
pushpiinder <pushpinder1997.ps@gmail.com>
2024-02-05 03:04:19 +0530

    fetch TMDB data & put it in store

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -36,7 +37,7 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
               //bug fix
